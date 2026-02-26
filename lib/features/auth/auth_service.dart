daa953c62e3ff3a541df229d4df757fd78c9154c
Ha Yueun <0308yueun@handong.ac.kr>
2026-02-26 09:37:09 +0900

    feat: save survey and rules to firestore

--- lib/features/auth/auth_service.dart
+++ lib/features/auth/auth_service.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../../models/user_model.dart';
 
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> saveUserToFirestore(User user, String nickname) async {
@@ -11,15 +10,26 @@ class AuthService {
         _firestore.collection('users').doc(user.uid);
 
     final docSnapshot = await userDoc.get();
+    final UserModel newUser = UserModel(
+      uid: user.uid,
+      email: user.email ?? '',
+      nickname: nickname,
+      photoUrl: user.photoURL,
+    );
 
     if (!docSnapshot.exists) {
-      final newUser = UserModel(
-        uid: user.uid,
-        email: user.email ?? '',
-        nickname: nickname,
-      );
-
       await userDoc.set(newUser.toMap());
+      return;
     }
+
+    await userDoc.set(
+      <String, dynamic>{
+        'email': user.email ?? '',
+        'nickname': nickname,
+        'photoUrl': user.photoURL,
+        'lastLoginAt': FieldValue.serverTimestamp(),
+      },
+      SetOptions(merge: true),
+    );
   }
 }
--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,12 +2,14 @@ class UserModel {
   final String uid;
   final String email;
   final String nickname;
+  final String? photoUrl;
   final String? roomId;
 
   UserModel({
     required this.uid,
     required this.email,
     required this.nickname,
+    this.photoUrl,
     this.roomId,
   });
 
@@ -16,6 +18,7 @@ class UserModel {
       'uid': uid,
       'email': email,
       'nickname': nickname,
+      'photoUrl': photoUrl,
       'roomId': roomId,
       'createdAt': DateTime.now(),
     };
@@ -26,6 +29,7 @@ class UserModel {
       uid: map['uid'],
       email: map['email'],
       nickname: map['nickname'],
+      photoUrl: map['photoUrl'],
       roomId: map['roomId'],
     );
   }
